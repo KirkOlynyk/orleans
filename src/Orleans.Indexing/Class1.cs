a55b0817967142d8f79628c6c9c5b843cb5037f9
REDMOND\kirko <kirko@microsoft.com>
2017-12-20 13:01:04 -0800

    - modified Orleans.Indexing.Class1 to have a stub function that returns the string that you passed it - Modified Samples/HelloWorld.NetCore/program.cs to call this function - Modified Samples/HelloWorld.NetCore to use the Orleans runtime that is built under src - Ported some interfaces to Orleans.Indexing

--- src/Orleans.Indexing/Class1.cs
+++ src/Orleans.Indexing/Class1.cs
@@ -4,5 +4,14 @@ namespace Orleans.Indexing
 {
     public class Class1
     {
+        /// <summary>
+        /// Stub function for testing. The string identity function.
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <returns>Output string the same object as the input string</returns>
+        public static string StringId(string input)
+        {
+            return input;
+        }
     }
 }
