430af261df4034932b7d077664730ad4afefb4da
REDMOND\kirko <kirko@microsoft.com>
2018-01-30 11:42:23 -0800

    Added client output under xunit test harness

--- test/Orleans.Indexing.Tests/Class1.cs
+++ test/Orleans.Indexing.Tests/Class1.cs
@@ -4,15 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Orleans.Indexing.Tests
 {
     public class Class1
     {
-        [Fact]
-        public static void PassingTest()
+        private readonly ITestOutputHelper output;
+
+        public Class1(ITestOutputHelper output)
         {
-            Console.WriteLine("First Indexing Test!");
+            this.output = output;
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            output.WriteLine("First Indexing Test!");
+            string assembly_file_path = @"D:\Orleans\orleans\src\Orleans.Indexing\bin\Debug\netstandard2.0\Orleans.Indexing.dll";
+            if (System.IO.File.Exists(assembly_file_path))
+            {
+                System.Reflection.Assembly asm = System.Reflection.Assembly.LoadFrom(assembly_file_path);
+                System.Type[] types = asm.GetTypes();
+                foreach (Type t in types)
+                {
+                    output.WriteLine(t.Name);
+                }
+            }
             Assert.Equal(1, 1);
         }
     }
